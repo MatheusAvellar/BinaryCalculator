0b0e22fdf17bb4d8e43b74e795bd4b39c75d3ac6
Matheus Avellar <Tetheu98@users.noreply.github.com>
2015-05-15 08:26:41 -0300

    Added subtraction to JS
    Will update the Python code soon

--- binCalc.js
+++ binCalc.js
@@ -1,24 +1,22 @@
 /*
  * Script by Matheus Avellar
  *   Binary Calculator
- *
- *                  fixNums()
- *                     |
- *                    for
- *
  */
 
 
-var n1 = "100"; // Binary numbers go here
-var n2 = "100"; // And here
+var n1 = "1"; // Binary numbers go here
+var n2 = "110110"; // And here
 var ex = 0;
+var opr = "+"; // Operation ("+" or "-") goes here
+var neg = " ";
 var res = "";
 
 (function fixNums() {
-	var max = n1.length > n2.length ? 2 : n2.length > n1.length ? 1 : "";
+	var max = n1.length > n2.length ? 2 : n2.length > n1.length ? 1 : 3;
 	switch (max) {
 		case 1:  n1 = "0" + n1; fixNums(); break;
 		case 2:  n2 = "0" + n2; fixNums(); break;
+		case 3:  if (opr == "-" && parseInt(n1) < parseInt(n2)){  invert(); neg = "-"  } break;
 		default: break;
 	}
 })();
@@ -26,15 +24,42 @@ var res = "";
 function _1() {  res = "1" + res;  }
 function _0() {  res = "0" + res;  }
 
-for (var i = n1.length - 1; i >= 0; i--) {
-	var e = parseInt(n1[i]) + parseInt(n2[i]) + ex;
-	switch (e) {
-		case 3:   _1(); ex = 1; break;
-		case 2:   _0(); ex = 1; break;
-		case 1:   _1(); ex = 0; break;
-		default:  _0(); ex = 0; break;
-	}
-	if (!i && ex) {  _1();  }
-};
+function invert() {
+	var n3 = n1;  // In the new JS, you could simply do
+	n1 = n2;      //    [n1, n2] = [n2, n1];
+	n2 = n3;      // But this ain't the new JS
+}
 
-// res now contains the result //
+(function result() {
+	if (opr == "+") {
+		for (var i = n1.length - 1; i >= 0; i--) {
+			var e = parseInt(n1[i]) + parseInt(n2[i]) + ex;
+			switch (e) {
+				case 3:   _1(); ex = 1; break;
+				case 2:   _0(); ex = 1; break;
+				case 1:   _1(); ex = 0; break;
+				default:  _0(); ex = 0; break;
+			}
+			if (!i && ex) {  _1();  }
+		};
+	}else if (opr == "-") {
+		for (var i = n1.length - 1; i >= 0; i--) {
+			var e = parseInt(n1[i]) - parseInt(n2[i]) - ex;
+			switch (e) {
+				case -2:  _0(); ex = 1; break;
+				case -1:  _1(); ex = 1; break;
+				case 0:   _0(); ex = 0; break;
+				case 1:   _1(); ex = 0; break;
+				default: break;
+			}
+			if (!i && ex) {  _1();  }
+		};
+	}else {
+		throw new TypeError('Use either "+" or "-" as the opr value');
+	}
+})();
+
+
+if (neg != " "){  invert(); res = neg + res;  }
+
+console.log(" " + n1 + "\n" + opr + n2 + "\n" + res);
