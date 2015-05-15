af3c3a95a2db114f674470539a1b1a059e068581
Matheus Avellar <Tetheu98@users.noreply.github.com>
2015-05-15 14:52:34 -0300

    Added actual result logs to .js

--- binCalc.js
+++ binCalc.js
@@ -4,62 +4,66 @@
  */
 
 
-var n1 = "1"; // Binary numbers go here
-var n2 = "110110"; // And here
+var n1 = "110";       // Binary numbers go here
+var n2 = "110";  // And here
 var ex = 0;
-var opr = "+"; // Operation ("+" or "-") goes here
+var opr = "+";      // Operation ("+" or "-") goes here
 var neg = " ";
 var res = "";
 
 (function fixNums() {
-	var max = n1.length > n2.length ? 2 : n2.length > n1.length ? 1 : 3;
-	switch (max) {
-		case 1:  n1 = "0" + n1; fixNums(); break;
-		case 2:  n2 = "0" + n2; fixNums(); break;
-		case 3:  if (opr == "-" && parseInt(n1) < parseInt(n2)){  invert(); neg = "-"  } break;
-		default: break;
-	}
+    var max = n1.length > n2.length ? 2 : n2.length > n1.length ? 1 : 3;
+    switch (max) {
+        case 1:  n1 = "0" + n1; fixNums(); break;
+        case 2:  n2 = "0" + n2; fixNums(); break;
+        case 3:  if (opr == "-" && parseInt(n1) < parseInt(n2)){  invert(); neg = "-"  } break;
+        default: break;
+    }
 })();
 
 function _1() {  res = "1" + res;  }
 function _0() {  res = "0" + res;  }
 
 function invert() {
-	var n3 = n1;  // In the new JS, you could simply do
-	n1 = n2;      //    [n1, n2] = [n2, n1];
-	n2 = n3;      // But this ain't the new JS
+    var n3 = n1;    // In the new JS, you could simply do
+    n1 = n2;        //    [n1, n2] = [n2, n1];
+    n2 = n3;        // But this ain't the new JS
 }
 
 (function result() {
-	if (opr == "+") {
-		for (var i = n1.length - 1; i >= 0; i--) {
-			var e = parseInt(n1[i]) + parseInt(n2[i]) + ex;
-			switch (e) {
-				case 3:   _1(); ex = 1; break;
-				case 2:   _0(); ex = 1; break;
-				case 1:   _1(); ex = 0; break;
-				default:  _0(); ex = 0; break;
-			}
-			if (!i && ex) {  _1();  }
-		};
-	}else if (opr == "-") {
-		for (var i = n1.length - 1; i >= 0; i--) {
-			var e = parseInt(n1[i]) - parseInt(n2[i]) - ex;
-			switch (e) {
-				case -2:  _0(); ex = 1; break;
-				case -1:  _1(); ex = 1; break;
-				case 0:   _0(); ex = 0; break;
-				case 1:   _1(); ex = 0; break;
-				default: break;
-			}
-			if (!i && ex) {  _1();  }
-		};
-	}else {
-		throw new TypeError('Use either "+" or "-" as the opr value');
-	}
+    if (opr == "+") {
+        for (var i = n1.length - 1; i >= 0; i--) {
+            var e = parseInt(n1[i]) + parseInt(n2[i]) + ex;
+            switch (e) {
+                case 3:   _1(); ex = 1; break;
+                case 2:   _0(); ex = 1; break;
+                case 1:   _1(); ex = 0; break;
+                default:  _0(); ex = 0; break;
+            }
+            if (!i && ex) {  _1();  }
+        };
+    }else if (opr == "-") {
+        for (var i = n1.length - 1; i >= 0; i--) {
+            var e = parseInt(n1[i]) - parseInt(n2[i]) - ex;
+            switch (e) {
+                case -2:  _0(); ex = 1; break;
+                case -1:  _1(); ex = 1; break;
+                case 0:   _0(); ex = 0; break;
+                case 1:   _1(); ex = 0; break;
+                default: break;
+            }
+            if (!i && ex) {  _1();  }
+        };
+    }else {
+        throw new TypeError('Use either "+" or "-" as the opr value');
+    }
 })();
 
 
 if (neg != " "){  invert(); res = neg + res;  }
-
-console.log(" " + n1 + "\n" + opr + n2 + "\n" + res);
+function c(number, baseFrom, baseTo) {
+    return parseInt(number, baseFrom).toString(baseTo);
+}
+console.log(n1 + "\n" + opr + n2 + "\n" + res);                               // Binary result
+console.log(c(n1, 2, 10) + "\n" + opr + c(n2, 2, 10) + "\n" + c(res, 2, 10)); // Decimal result
+console.log(c(n1, 2, 16) + "\n" + opr + c(n2, 2, 16) + "\n" + c(res, 2, 16)); // Hexadecimal result
